b6a1194b7fc41e3e0c6fae03fc2b262eb2025edf
gbchoi <mosfet1kg@gmail.com>
2015-08-22 03:00:01 +0900

    client에서 buffer를 통해 stream 뿌리는것 추가

--- example/develop.js
+++ example/develop.js
@@ -1,14 +1,62 @@
-'use strict';
+// node v0.10.39에서 최적화
 
-var frambuffer = require('../lib/framebuffer');
 
-var fb = new frambuffer('/dev/fb0');
 
-console.log(fb.toString());
-console.log(fb.buffer().length/4);
+var framebuffer = require('../lib/framebuffer');
+var express = require('express');
+var fb = new framebuffer('/dev/fb0');
+var path = require('path');
 
-for(var i=0; i< 30; i++) {
-    console.log(fb.buffer().readUInt8(i));
-}
+var app = express();
+var server = app.listen( process.env.port || 55555, function(){
+    console.log('This server is running on the port ' + this.address().port );
+});
 
-fb.close();
+
+
+var io = require('socket.io').listen(server);
+app.use( express.static( path.join(__dirname,'..', 'public') ));
+
+var fb_tmp = fb.buffer();
+
+//console.log(fb_tmp);
+//var imgArray = new Uint8Array(fb_tmp);
+//console.log(fb_tmp.toString('base64'));
+
+//var imgArray = new Uint8Array(fb_tmp);
+//console.log(imgArray.length);
+
+//fs.readFile(path.join(__dirname,'..','public','screenshot.png'), function(err, buf){
+//    console.log(buf);
+//});
+
+
+io.sockets.on('connection', function(socket){
+
+
+    socket.on('publish', function(){
+
+        //imgArray.slice(0,400)
+        socket.emit('canvas', {image: true, buffer: fb_tmp });
+        //var fb_tmp = fb.buffer();
+
+        //socket.emit('canvas', {image:true, buffer: new Uint8Array(fb_tmp)});
+
+
+        ////console.log(fb_tmp.toSting());
+        ////console.log(fb_tmp.buffer().length/4);
+        //
+
+
+        //for(var i=0; i< fb_tmp.length; i++) {
+        //    //socket.emit('canvas', fb_tmp.readUInt8(i));
+        //    console.log(i);
+        //    fb_tmp.readUInt8(i);
+        //}
+
+    });
+
+
+});
+
+//fb.close();
